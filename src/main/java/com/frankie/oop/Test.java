f28c42ed681362efb343648e3341f50687dd95b9
yycer <15900617310@163.com>
2020-09-03 15:07:32 +0800

    Initial commit

---
+++ src/main/java/com/frankie/oop/Test.java
@@ -0,0 +1,12 @@
+package com.frankie.oop;
+
+/**
+ * @author: Yao Frankie
+ * @date: 2020/9/3 15:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
